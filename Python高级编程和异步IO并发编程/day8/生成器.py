57a78a46ac9306be8bcac48923084f3572c63947
chenzwcc <2552770507@qq.com>
2019-07-17 10:17:21 +0800

    更新day8内容

--- Python高级编程和异步IO并发编程/day8/生成器.py
+++ Python高级编程和异步IO并发编程/day8/生成器.py
@@ -3,7 +3,33 @@
 # 创建日期  ：2019/7/16 下午4:14
 
 """
-生成器函数:只要函数内部包含yield关键字，那么这个函数就是生成器函数
+    迭代器原理：只需要在类的__iter__方法中返回一个对象自身，这个对象拥有一个next()方法，这个方法能不断返回下一个值并在恰当的时候抛出StopIteration异常即可
+"""
+
+
+# 自定义迭代器
+
+
+class CustomIteration(object):
+
+    def __init__(self, data_list):
+        self.data_list = data_list
+        self.num = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.num < len(self.data_list):
+            self.num += 1
+            return self.data_list[self.num - 1]
+        else:
+            raise StopIteration
+
+
+"""
+    生成器的编写方法和函数定义类似，只是在return的地方改为yield,当生成器遇到一个yield时，会暂停运行生成器，
+    返回yield后面的值。当再次调用生成器的时候，会从刚才暂停的地方继续运行，直到下一个yield
 """
 
 
@@ -11,10 +37,14 @@ def func():
     yield 1
     yield 2
     yield 3
-    yield 4
 
 
 if __name__ == '__main__':
-    fun = func()
-    for i in fun:
+    a = CustomIteration([1, 2, 3])
+    print(type(a), a)
+    for i in a:
         print(i)
+
+    for i in func():
+        print(i)
+
